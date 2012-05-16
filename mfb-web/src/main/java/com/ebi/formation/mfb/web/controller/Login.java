a434e0a0e3d8dbabae1649b55776ffca17d28698
Kevin Pogorzelski <kpogorzelski@excilys.com>
2012-05-16 12:20:06 +0200

    Add javadoc to class.

--- mfb-web/src/main/java/com/ebi/formation/mfb/web/controller/Login.java
+++ mfb-web/src/main/java/com/ebi/formation/mfb/web/controller/Login.java
@@ -3,9 +3,16 @@ package com.ebi.formation.mfb.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @author kpogorzelski
+ * 
+ */
 @Controller
 public class Login {
 
+	/**
+	 * @return String
+	 */
 	@RequestMapping("/login.php")
 	public String redirect() {
 		return "login";
--- mfb-web/src/main/java/com/ebi/formation/mfb/web/controller/errors/Errors.java
+++ mfb-web/src/main/java/com/ebi/formation/mfb/web/controller/errors/Errors.java
@@ -3,14 +3,24 @@ package com.ebi.formation.mfb.web.controller.errors;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @author kpogorzelski
+ * 
+ */
 @Controller
 public class Errors {
 
+	/**
+	 * @return String
+	 */
 	@RequestMapping("404.php")
 	public String error404() {
 		return "page404";
 	}
 
+	/**
+	 * @return String
+	 */
 	@RequestMapping("500.php")
 	public String error500() {
 		return "page500";
