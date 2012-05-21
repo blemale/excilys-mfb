52b4d84c97568a1c3cc1226cf3bd84f4830f63e1
Bastien LEMALE <blemale@excilys.com>
2012-05-21 11:22:41 +0200

    Redirection après login réussi (Controllers)

--- mfb-web/src/main/java/com/ebi/formation/mfb/web/controller/Admin.java
+++ mfb-web/src/main/java/com/ebi/formation/mfb/web/controller/Admin.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
+ * Controller gérant l'accès à la partie admin de l'application.
+ * 
  * @author kpogorzelski
  * 
  */
@@ -13,7 +15,7 @@ public class Admin {
 	/**
 	 * @return String
 	 */
-	@RequestMapping("admin.html")
+	@RequestMapping("/admin/home.html")
 	public String redirect() {
 		return "admin";
 	}
--- mfb-web/src/main/java/com/ebi/formation/mfb/web/controller/Home.java
+++ mfb-web/src/main/java/com/ebi/formation/mfb/web/controller/Home.java
@@ -1,9 +1,16 @@
 package com.ebi.formation.mfb.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ebi.formation.mfb.entities.Role.Right;
 
 /**
+ * Controller qui gère l'accès aux différentes pages home.html suivant les droits de l'utilisateur.
+ * 
  * @author kpogorzelski
  * 
  */
@@ -13,8 +20,14 @@ public class Home {
 	/**
 	 * @return String
 	 */
-	@RequestMapping("home.html")
-	public String redirect() {
-		return "home";
+	@RequestMapping(value = "home.html", method = RequestMethod.GET)
+	public String redirect(HttpServletRequest request) {
+		if (request.isUserInRole(Right.ROLE_CLIENT.name())) {
+			return "redirect:/client/home.html";
+		} else if (request.isUserInRole(Right.ROLE_ADMIN.name())) {
+			return "redirect:/admin/home.html";
+		} else {
+			return "redirect:/login.html";
+		}
 	}
 }
