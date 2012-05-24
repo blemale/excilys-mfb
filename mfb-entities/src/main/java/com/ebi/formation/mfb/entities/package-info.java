d2208255d5abd0c83552672bea1f780eb9eec15d
Kevin Pogorzelski <kpogorzelski@excilys.com>
2012-05-24 16:59:12 +0200

    Création de l'entité Operation, du DAO associé, des tests associés, et ajout d'une nouvelle version de db.

--- mfb-entities/src/main/java/com/ebi/formation/mfb/entities/package-info.java
+++ mfb-entities/src/main/java/com/ebi/formation/mfb/entities/package-info.java
@@ -2,7 +2,15 @@
 @NamedQueries({
 		@NamedQuery(name = "findUserDetailsByUsername", query = "SELECT p FROM Person p WHERE p.username = :username"),
 		@NamedQuery(name = "findComptesByUsername", query = "SELECT p.comptes FROM Person p WHERE p.username = :username"),
-		@NamedQuery(name = "findPersonByUsername", query = "SELECT p FROM Person p WHERE p.username = :username") })
+		@NamedQuery(name = "findPersonByUsername", query = "SELECT p FROM Person p WHERE p.username = :username"),
+		@NamedQuery(name = "findTotalOperationsCarteByMonth", query = "SELECT SUM(o.montant) FROM Operation o WHERE o.type.label = :type AND o.compte.id = :idcompte "
+				+ "and o.dateEffet BETWEEN :dateEffet AND :datePlusUnMois"),
+		@NamedQuery(name = "findNumberOfOperationsCarteByMonth", query = "SELECT COUNT(o) FROM Operation o WHERE o.type.label = :type AND o.compte.id = :idcompte "
+				+ "and o.dateEffet BETWEEN :dateEffet AND :datePlusUnMois"),
+		@NamedQuery(name = "findOperationsWithoutCarteByMonthPaginated", query = "SELECT o FROM Operation o WHERE o.type.label <> 'CARTE' AND o.compte.id = :idcompte "
+				+ "and o.dateEffet BETWEEN :dateEffet AND :datePlusUnMois ORDER BY o.dateEffet"),
+		@NamedQuery(name = "findOperationsCarteByMonthPaginated", query = "SELECT o FROM Operation o WHERE o.type.label = 'CARTE' AND o.compte.id = :idcompte "
+				+ "and o.dateEffet BETWEEN :dateEffet AND :datePlusUnMois ORDER BY o.dateEffet") })
 package com.ebi.formation.mfb.entities;
 
 import org.hibernate.annotations.NamedQueries;
