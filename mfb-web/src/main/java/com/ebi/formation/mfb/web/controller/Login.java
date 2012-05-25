aec6ffc542232c7967d02e5e2d67490475ef1161
fguillain <fguillain@excilys.com>
2012-05-25 12:18:31 +0200

    Implémentation du front le détail des opérations carte d'un compte !

--- mfb-web/src/main/java/com/ebi/formation/mfb/web/controller/Login.java
+++ mfb-web/src/main/java/com/ebi/formation/mfb/web/controller/Login.java
@@ -1,8 +1,12 @@
 package com.ebi.formation.mfb.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ebi.formation.mfb.entities.Role.Right;
 
 /**
  * @author kpogorzelski
@@ -15,7 +19,13 @@ public class Login {
 	 * @return String
 	 */
 	@RequestMapping(value = "login.html", method = RequestMethod.GET)
-	public String redirect() {
-		return "login";
+	public String redirect(HttpServletRequest request) {
+		if (request.isUserInRole(Right.ROLE_CLIENT.name())) {
+			return "redirect:/client/home.html";
+		} else if (request.isUserInRole(Right.ROLE_ADMIN.name())) {
+			return "redirect:/admin/home.html";
+		} else {
+			return "login";
+		}
 	}
 }
